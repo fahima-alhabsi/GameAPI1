0f7d200c48cc403785df641837ef2437428b4162
unknown <fahima-alhabsi>
2023-07-23 22:43:39 +0400

    preparing the annotations in the classes

--- src/main/java/com/example/GameAPI/Controllers/playerCotroller.java
+++ src/main/java/com/example/GameAPI/Controllers/playerCotroller.java
@@ -1,6 +1,13 @@
 package com.example.GameAPI.Controllers;
 
+import com.example.GameAPI.Servises.playerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class playerCotroller {
+    @Autowired
+    playerService PlayerService;
+
+
 }
--- src/main/java/com/example/GameAPI/Servises/playerService.java
+++ src/main/java/com/example/GameAPI/Servises/playerService.java
@@ -1,4 +1,11 @@
 package com.example.GameAPI.Servises;
 
+import com.example.GameAPI.Repositries.playerRepositry;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class playerService {
+    @Autowired
+    playerRepositry PlayerRepositry;
 }
